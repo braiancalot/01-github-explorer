d5881b80f44487a9f9f778e8e0da86dcf3c26f3e
braiancalot <calot665@hotmail.com>
2022-03-28 22:28:38 -0300

    finalizando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { RepositoryList } from './components/RepositoryList'
-import { Counter } from './components/Counter'
 import './styles/global.scss'
+import './styles/repositories.scss'
 
 export function App() {
 
     return (
         <>
             <RepositoryList />
-            <Counter />
         </>
     )
 }
